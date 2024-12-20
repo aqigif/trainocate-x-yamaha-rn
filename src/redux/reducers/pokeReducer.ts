71019c02f75cf98c2b465e0d248574fdf874c5c2
Aqil G <aqilgifari0@gmail.com>
2024-12-20 14:20:17 +0700

    fix: remove add and delete

--- src/redux/reducers/pokeReducer.ts
+++ src/redux/reducers/pokeReducer.ts
@@ -2,8 +2,6 @@ import {AnyAction} from '@reduxjs/toolkit';
 import {IPokemon} from '../../types/pokemon';
 import {
   POKEMONS,
-  ADD_TO_MY_POKEMON,
-  DELETE_FROM_MY_POKEMON,
 } from '../actionTypes';
 
 interface IStore {
@@ -22,17 +20,6 @@ export const pokeReducer = (state = initialState, action: AnyAction) => {
   const payload = action?.payload as unknown as IStore;
 
   switch (action.type) {
-    case ADD_TO_MY_POKEMON:
-      return {
-        ...state,
-        pokemons: payload.pokemons,
-      };
-
-    case DELETE_FROM_MY_POKEMON:
-      return {
-        ...state,
-        pokemons: payload.pokemons,
-      };
     case POKEMONS.GET_POKEMONS_REQUEST:
       return {
         ...state,
