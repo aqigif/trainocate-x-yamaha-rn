ec097ec2ab7a1c7fb75bc5722b087c176a6f9ec1
Aqil G <aqilgifari0@gmail.com>
2024-12-20 14:20:07 +0700

    fix: camera position

--- src/screens/CameraScreen/CameraScreen.tsx
+++ src/screens/CameraScreen/CameraScreen.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {
   Camera,
   useCameraDevice,
   useCameraPermission,
 } from 'react-native-vision-camera';
 import CameraPermission from './components/CameraPermission';
+import IconButton from '../../components/IconButton';
+import Icon from '@react-native-vector-icons/fontawesome6';
 
 function CameraScreen() {
-  const device = useCameraDevice('front');
+  const [cameraPosition, setCameraPosition] = useState<'back' | 'front'>(
+    'back',
+  );
+  const device = useCameraDevice(cameraPosition);
   const {hasPermission} = useCameraPermission();
 
   if (!hasPermission) {
@@ -18,7 +23,19 @@ function CameraScreen() {
     return <Text> no camera </Text>;
   }
   return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <>
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+      <View style={{position: 'absolute', bottom: 50, right: 20}}>
+        <IconButton
+          size={60}
+          backgroundColor={'black'}
+          onPress={() =>
+            setCameraPosition(prev => (prev === 'back' ? 'front' : 'back'))
+          }>
+          <Icon iconStyle="solid" name="camera-rotate" size={20} color={'white'} />
+        </IconButton>
+      </View>
+    </>
   );
 }
 
